dfe5fb18aafe31f030c8c5380e71d788abb4a34e
Tom Plant <tom@tplant.com.au>
2024-11-17 06:44:20 +0000

    Add vial support for yunomi
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- keyboards/yunomi/config.h
+++ keyboards/yunomi/config.h
@@ -3,23 +3,13 @@
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
-#define DEVICE_VER   0x0001
-#define MANUFACTURER Google
-#define PRODUCT      yunomi
-
 /* key matrix size */
 #define MATRIX_ROWS 11
 #define MATRIX_COLS 11
 
 /* Keyboard Matrix Assignments */
-#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4, D0, D1, D2, D3, D4, D5 }
-#define MATRIX_COL_PINS { D0, D1, D2, D3, D4, D5, B0, B1, B2, B3, B4 }
-#define UNUSED_PINS
+#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6, B5, B4, E6, D7, C6, D4 }
+#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4, F7, B1, B3, B2, B6 }
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION ROW2COL
@@ -31,7 +21,3 @@
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
-
-/* disable these deprecated features by default */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
